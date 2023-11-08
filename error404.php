b7d6783dabb86d6ebef04e84312b0a0ab4d9a696
rafitojuan <rafitosch@gmail.com>
2023-11-08 07:32:46 +0700

    ERROR 404 ADDED

---
+++ error404.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ERROR 404</title>
+    <link rel="shortcut icon" href="user/asset/img/SEMUDAH-LOGO.png" type="image/x-icon">
+    <link rel="stylesheet" href="user/asset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="user/asset/css/style.css">
+    <link href="user/asset/aos/aos.css" rel="stylesheet">
+</head>
+
+<style>
+    .error1 {
+        font-size: 50px;
+    }
+</style>
+
+<body>
+
+    <div class="text-center">
+
+        <div class="flex">
+
+            <div class="col eror-text">
+                <h1 class="error1">404</h1>
+                <h5>PAGE NOT FOUND</h5>
+                <p>The page you are looking for doesn't exist or an other error occurred<br>
+                    Go back, or head over to semudah.com to choose a new direction. </p>
+            </div>
+
+            <div class="col2">
+                <img class="imgeror" src="user/asset/img/depan.png" width="600px" alt="">
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
